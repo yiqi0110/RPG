6748deb5e8a804c2c3e29529ca56f38228513f13
yiqi0110 <lili-rovert@hotmail.com>
2018-08-11 15:10:43 -0600

    for claass

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,15 +1,15 @@
 //creatation of the four characters objects
 // Attack power for lichking
-var lichAttk = 50;
+var lichAttk = 40;
 
 // Attack power for stormrage
-var stormAttk = 75;
+var stormAttk = 95;
 
 // Attack power for windrunner
-var windAttk = 25;
+var windAttk = 20;
 
 // Attack power for thrall
-var thrallAttk = 20;
+var thrallAttk = 35;
 
 var lichking = {
     name: "Arthus, The Lich King",
@@ -40,6 +40,14 @@ var thrall = {
 };
 // console.log(lichking, stormrage, windrunner, thrall);
 
+var attack = function (x, y) {
+    y.hp = y.hp - x.attk;
+    x.attk = x.attk + x.attk;
+    if (y.hp >= 0) {
+        x.hp = x.hp - y.attk;
+    };
+    console.log("My attack function " + x,y);
+};
 
 
 
@@ -63,6 +71,7 @@ function startup() {
         // Creates a variable for the #_player and #_opponent
         var playerCharacter;
         var opponentCharacter;
+        var defeatedCharacters = [];
         
         // Defining the variables for the attack function below
         var y;
@@ -104,92 +113,141 @@ function startup() {
             if ($('#_player').find('div#lichdiv').length !== 0) {
                 playerCharacter = lichking;
                 x = playerCharacter;
-                console.log(x);
+                // console.log(x);
             };
             if ($('#_player').find('div#stormdiv').length !== 0) {
                 playerCharacter = stormrage;
                 x = playerCharacter;
-                console.log(playerCharacter)
+                // console.log(playerCharacter)
+                 
             };
             if ($('#_player').find('div#winddiv').length !== 0) {
                 playerCharacter = windrunner;
                 x = playerCharacter;
-                console.log(playerCharacter);
+                // console.log(playerCharacter);
+                 
             };
             if ($('#_player').find('div#thralldiv').length !== 0) {
                 playerCharacter = thrall;
                 x = playerCharacter;
-                console.log(playerCharacter);
+                // console.log(playerCharacter);
+                 
             };
+
+            // opponent select
+            $('#_enemies').click(function (event) {
+                if (event.target.id !== 'lichimg') {} else {
+                    $(this).hide();
+                    $('#lichdiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
+                    $('#lichimg').attr('style', 'border: 5px solid red');
+                    $('#_player').prependTo('#_battleground');
+                };
+                if (event.target.id !== 'stormimg') {} else {
+                    $(this).hide();
+                    $('#stormdiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
+                    $('#stormimg').attr('style', 'border: 5px solid red');
+                    $('#_player').prependTo('#_battleground');
+                };
+                if (event.target.id !== 'windimg') {} else {
+                    $(this).hide();
+                    $('#winddiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
+                    $('#windimg').attr('style', 'border: 5px solid red');
+                    $('#_player').prependTo('#_battleground');
+                };
+                if (event.target.id !== 'thrallimg') {} else {
+                    $(this).hide();
+                    $('#thralldiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
+                    $('#thrallimg').attr('style', 'border: 5px solid red');
+                    $('#_player').prependTo('#_battleground');
+                    
+                };
+                // opponent storage
+                if ($('#_opponent').find('div#lichdiv').length !== 0) {
+                    opponentCharacter = lichking;
+                    y = opponentCharacter;
+                };
+                if ($('#_opponent').find('div#stormdiv').length !== 0) {
+                    opponentCharacter = stormrage;
+                    y = opponentCharacter;
+                    // console.log(opponentCharacter);
+                     
+                };
+                if ($('#_opponent').find('div#winddiv').length !== 0) {
+                    opponentCharacter = windrunner;
+                    y = opponentCharacter;
+                    // console.log(opponentCharacter);
+                     
+                };
+                if ($('#_opponent').find('div#thralldiv').length !== 0) {
+                    opponentCharacter = thrall;
+                    y = opponentCharacter;
+                    // console.log(opponentCharacter);
+                     
+                };
+                
+                $('#_gameTime').attr('style', 'color: red; font-size: 100px');
+                console.log("This is the selection " +x,y);
+                $('#_attack').show();
+                var wins = 0;
+                $('#_attack').click(function () {
+                    console.log("somthithiasdfafdsbdfs");
+                    attack(x,y);
+                    if (y.hp < 0) {
+            
+                        console.log("you killed " + y.name + "!");
+
+                        
+                        if ($('#_battleground').find('div#lichdiv').length !== 0) {
+                            $('#_opponent').hide().appendTo('#_defeated');
+                            wins++;
+                            $('#_enemies').show();
+                            $('#_player').appendTo('#_playerHolder');
+                            $('#_gameTime').attr('style', 'font-size: 1px');
+                        }
+                        if ($('#_battleground').find('div#stormdiv').length !== 0) {
+                            $('#_opponent').hide().appendTo('#_defeated');
+                            wins++;
+                            $('#_enemies').show();
+                            $('#_player').appendTo('#_playerHolder');
+                            $('#_gameTime').attr('style', 'font-size: 1px');
+                        }
+                        if ($('#_battleground').find('div#winddiv').length !== 0) {
+                            $('#_opponent').hide().appendTo('#_defeated');
+                            wins++;
+                            $('#_enemies').show();
+                            $('#_player').appendTo('#_playerHolder');
+                            $('#_gameTime').attr('style', 'font-size: 1px');
+                        }
+                        if ($('#_battleground').find('div#thralldiv').length !== 0) {
+                            $('#_opponent').hide().appendTo('#_defeated');
+                            wins++;
+                            $('#_enemies').show();
+                            $('#_player').appendTo('#_playerHolder');
+                            $('#_gameTime').attr('style', 'font-size: 1px');
+                        }
+                        y = ''
+                        console.log("this is the y after death" + y);
+                        console.log(wins);
+                        // if all opponents defeated then victory
+                        if (wins === 3) {
+                            console.log('fdf');
+                        }
+                    }
+                });
+
+            });
         });
-        
         // Allows you to select your combatant then moves players character and combatant to PVP
         // As well as gives the player and 
-        $('#_enemies').click(function (event) {
-            if (event.target.id !== 'lichimg') {} else {
-                $(this).hide();
-                $('#lichdiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
-                $('#lichimg').attr('style', 'border: 5px solid red');
-                $('#_player').prependTo('#_battleground');
-            };
-            if (event.target.id !== 'stormimg') {} else {
-                $(this).hide();
-                $('#stormdiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
-                $('#stormimg').attr('style', 'border: 5px solid red');
-                $('#_player').prependTo('#_battleground');
-            };
-            if (event.target.id !== 'windimg') {} else {
-                $(this).hide();
-                $('#winddiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
-                $('#windimg').attr('style', 'border: 5px solid red');
-                $('#_player').prependTo('#_battleground');
-            };
-            if (event.target.id !== 'thrallimg') {} else {
-                $(this).hide();
-                $('#thralldiv').appendTo('#_battleground').wrap("<div id='_opponent'></div>");
-                $('#thrallimg').attr('style', 'border: 5px solid red');
-                $('#_player').prependTo('#_battleground');
-                
-            };
-            // opponent storage
-            if ($('#_opponent').find('div#lichdiv').length !== 0) {
-                opponentCharacter = lichking;
-                y = opponentCharacter;
-                console.log(y);
-            };
-            if ($('#_opponent').find('div#stormdiv').length !== 0) {
-                opponentCharacter = stormrage;
-                y = opponentCharacter;
-                // console.log(opponentCharacter);
-            };
-            if ($('#_opponent').find('div#winddiv').length !== 0) {
-                opponentCharacter = windrunner;
-                y = opponentCharacter;
-                console.log(opponentCharacter);
-            };
-            if ($('#_opponent').find('div#thralldiv').length !== 0) {
-                opponentCharacter = thrall;
-                y = opponentCharacter;
-                console.log(opponentCharacter);
-            };
-            console.log(this);
-            $('#_gameTime').attr('style', 'color: red; font-size: 100px');
-        });
-
+        
         // function to attack
-        var attack = function (x, y) {
-            y.hp = y.hp - x.attk;
-            x.attk = x.counterAttk;
-            if (y.hp >= 0) {
-                x.hp = x.hp - y.attk;
-            };
-        };
+        
 
-        $('#_attack').show();
+        // if ($('#_battleground:contains(x&&y)')) {
+        // attack();
+        // };
         // Now to comence the battle
         // $('#_attack').click(attack(x,y));
-        console.log(playerCharacter);
-        console.log(opponentCharacter);
         
         
     });
